ccb18c73d9e0c0e01d31c6c53f215576c08549ca
Evgeniy <evgen-z2007@yandex.ru>
2023-06-22 20:51:25 +0500

    Домашнее задание к 10 семинару

---
+++ fabrica.py
@@ -0,0 +1,18 @@
+from dz_seminar10 import Bird, Dog, Fish
+
+
+class AnimalFabric:
+    def make_animal(self, animal_type: str, *args, **kwargs):
+        new_animal = self._get_maker(animal_type)
+        return new_animal(*args, **kwargs)
+
+    def _get_maker(self, animal_type: str):
+        types = {"dog": Dog, "bird": Bird, "fish": Fish}
+        return types[animal_type.lower()]
+
+
+if __name__ == '__main__':
+    fabric = AnimalFabric()
+    animal_from_fabric = fabric.make_animal("dog", " Бим", 50, 3, "Лайка")
+    animal_from_fabric.commands = ["сидеть", "служить"]
+    print(animal_from_fabric)
